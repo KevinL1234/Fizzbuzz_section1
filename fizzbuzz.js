f22c0074ebd0b7e0b428fec9cf01ad872205bf37
Morgan Benton <morgan.benton@gmail.com>
2016-03-23 10:01:37 -0400

    added fizz, buzz, fizzbuzz tests and functions

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,4 +1,5 @@
-var count, fizzer;
+var count, fizzer, buzzer, fizzbuzzer;
+
 count = function(start, end) {
 	// create a variable for our output
 	var output = start;
@@ -6,18 +7,32 @@ count = function(start, end) {
 	// modify output to look like what we want
 	for (var i = start; i < end; i += 1) {
 		// code to be repeated goes here
-		output = output + " " + (i + 1);
+		output = output + " " + fizzer(i + 1);
 	}
 
-	// return the output, e.g "1 2 3 4 5"
+	// return the output, e.g "1 2 fizz 4 buzz"
 	return output;
 };
 
 fizzer = function(num) {
-	return "fizz";
+	// if (0 === num % 3) {
+	// 	return "fizz";
+	// }
+	// return num;
+	return 0 === num % 3 ? "fizz" : num;
+};
+
+buzzer = function(num) {
+	return 0 === num % 5 ? "buzz" : num;
+};
+
+fizzbuzzer = function(num) {
+	return 0 === num % 15 ? "fizzbuzz" : num;
 };
 
 module.exports = {
 	count: count,
-	fizzer: fizzer
+	fizzer: fizzer,
+	buzzer: buzzer,
+	fizzbuzzer: fizzbuzzer
 };
--- spec/fizzbuzz_spec.js
+++ spec/fizzbuzz_spec.js
@@ -2,8 +2,9 @@ var fb = require('../fizzbuzz');
 
 describe("A fizzbuzz program", function() {
 
-	it("can count from 1 to 5", function() {
-		expect(fb.count(1,5)).toBe("1 2 3 4 5");
+	it("can count from 1 to 5 and do fizzbuzz replacements", function() {
+		expect(fb.count(1,5)).toBe("1 2 fizz 4 buzz");
+		expect(fb.count(10,15)).toBe("buzz 11 fizz 13 14 fizzbuzz");
 	});
 
 	it("can take a number and return 'fizz' if it is a multiple of 3, and the number otherwise", function(){
@@ -13,4 +14,17 @@ describe("A fizzbuzz program", function() {
 		expect(fb.fizzer(17)).toBe(17);
 	});
 
+	it("can take a number and return 'buzz' if it is a multiple of 5, and the number otherwise", function(){
+		expect(fb.buzzer(5)).toBe('buzz');
+		expect(fb.buzzer(25)).toBe('buzz');
+		expect(fb.buzzer(3)).toBe(3);
+		expect(fb.buzzer(17)).toBe(17);
+	});
+
+	it("can take a number and return 'fizzbuzz' if it is a multiple of 15, and the number otherwise", function(){
+		expect(fb.fizzbuzzer(15)).toBe('fizzbuzz');
+		expect(fb.fizzbuzzer(45)).toBe('fizzbuzz');
+		expect(fb.fizzbuzzer(3)).toBe(3);
+		expect(fb.fizzbuzzer(17)).toBe(17);
+	});
 });
